3274ff6b6b5fc7396a64ca6d29a2ad1e6a5fc39e
raoní silla <Raonisilla@gmail.com>
2025-06-25 23:40:19 -0300

    update

---
+++ src/src/academy/devdojo/maratonaJavaIntroducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonaJavaIntroducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // 8 tipos de variaveis primitivas, int,double,float,char,byte,short,long,boolean
+        int variavelUm =10;
+        System.out.println("a idade é " + variavelUm + " anos");
+    }
+}
--- src/src/academy/devdojo/maratonaJavaIntroducao/OlaMundo.java
+++ src/src/academy/devdojo/maratonaJavaIntroducao/OlaMundo.java
@@ -1,4 +1,8 @@
 package academy.devdojo.maratonaJavaIntroducao;
+
+/**
+ *  isso é um comentario <h1>java doc</h1>
+ */
 //todas as classes dentro de pacotes devem começar com a linha de codigo sendo o pacote
 public class OlaMundo {
     public static void main (String [] args){
