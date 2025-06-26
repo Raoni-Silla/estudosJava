4518ee0826e23b84b130746068d682b758d3a2f5
raoní silla <Raonisilla@gmail.com>
2025-06-25 22:48:15 -0300

    update

---
+++ src/src/academy/devdojo/maratonaJavaIntroducao/OlaMundo.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonaJavaIntroducao;
+//todas as classes dentro de pacotes devem começar com a linha de codigo sendo o pacote
+public class OlaMundo {
+    public static void main (String [] args){
+        System.out.println("Hello,World");
+    }
+}
